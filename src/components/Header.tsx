9e14d7cb0f2a0e8c5d0084b4204669c4ba5c8699
Jawad Ullah <jawadullah156@gmail.com>
2023-09-07 22:45:00 -0700

    changed Logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -13,7 +13,7 @@ const Header: FC = () => {
     <nav className="bg-gray-600">
       <div className="max-w-7xl mx-auto flex items-center h-16 justify-between px-4 ">
         <div className="w-10 h-10 object-contain">
-          <img src="../../src/assets/logo.png" alt="logo" />
+          <img src="https://img.freepik.com/premium-vector/checklist-complete-tasks-todo-list-survey-exam-concepts-premium-quality-modern-flat-design_189959-533.jpg?w=2000" alt="logo" />
         </div>
         <div className="hidden md:block ">
           <Menu closeMenu={handleToggleMenu} />
